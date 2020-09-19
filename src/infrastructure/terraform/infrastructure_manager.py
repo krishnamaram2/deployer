e1a76248b70cc1c098953aa95d4b9c54338a7614
Cloud User <centos@ip-172-31-24-73.ec2.internal>
2020-09-19 08:28:45 +0000

    update

--- src/infrastructure/terraform/infrastructure_manager.py
+++ src/infrastructure/terraform/infrastructure_manager.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-cmds = ["sudo git clone https://github.com/krishnamaram2/infrastructure_manager.git && cd ./infrastructure_manager/src/webapp", "sudo terraform init ./infrastructure_manager/src/webapp/ && sudo terraform  validate ./infrastructure_manager/src/webapp  && sudo terraform apply -auto-approve  ./infrastructure_manager/src/webapp"]
+cmds = ["sudo git clone https://github.com/krishnamaram2/infrastructure-manager.git && cd ./infrastructure-manager/src/webapp", "sudo terraform init ./infrastructure-manager/src/webapp/ && sudo terraform  validate ./infrastructure-manager/src/webapp  && sudo terraform apply -auto-approve  ./infrastructure-manager/src/webapp"]
 
 for cmd in cmds:
     print("commnad to execute", cmd)
