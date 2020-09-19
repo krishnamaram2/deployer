c5be820191c6a7bb4a5d8c262da6d1c535611e81
Cloud User <centos@ip-172-31-24-73.ec2.internal>
2020-09-19 08:34:14 +0000

    update

--- src/infrastructure/packer/image_builder.py
+++ src/infrastructure/packer/image_builder.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-cmds = ["sudo git clone https://github.com/krishnamaram2/image_builder.git && cd ./image_builder/src", "packer validate -var-file=./image_builder/src/variables.json ./image_builder/src/builders.json && packer build -var-file=./image_builder/src/variables.json ./image_builder/src/builders.json"]
+cmds = ["sudo git clone https://github.com/krishnamaram2/image-builder.git && cd ./image-builder/src", "packer validate -var-file=./image-builder/src/variables.json ./image-builder/src/builders.json && packer build -var-file=./image-builder/src/variables.json ./image-builder/src/builders.json"]
 
 for cmd in cmds:
     print("commnad to execute", cmd)
